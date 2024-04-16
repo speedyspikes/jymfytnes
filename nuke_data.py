819a37d843b05c6fa7ab229c9afa07a3b2e1b4f1
SpeedySpikes <speedyspikes@gmail.com>
2024-04-16 10:17:01 -0600

    Fixed foreign key checks

--- nuke_data.py
+++ nuke_data.py
@@ -6,6 +6,9 @@ def nuke_data():
     conn = create_client()
     cursor = conn.cursor()
 
+    # Temporarily disable foreign key checks
+    cursor.execute("SET FOREIGN_KEY_CHECKS=0;")
+
     cursor.execute("DELETE FROM TRAINER")
     cursor.execute("DELETE FROM ROOM")
     cursor.execute("DELETE FROM TRAINER_TrainerSpecialty")
@@ -14,6 +17,9 @@ def nuke_data():
     cursor.execute("DELETE FROM CLASS")
     cursor.execute("DELETE FROM PERSONAL_TRAINING_SESSION")
     cursor.execute("DELETE FROM Class_Attendance")
+
+    # Re-enable foreign key checks
+    cursor.execute("SET FOREIGN_KEY_CHECKS=1;")
     
     conn.commit()
     print("All data deleted successfully.")
